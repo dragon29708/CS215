0c39f0af4f4d3b29f20c03c15569071631bee459
Andrew Cassidy <andrew.cassidy@uky.edu>
2019-09-24 00:05:01 -0400

    Test program for p1

---
+++ p1tester.cpp
@@ -0,0 +1,17 @@
+/*
+--------------------------------------------------------------------
+                        CS215-401 Program 1 TESTER
+--------------------------------------------------------------------
+Author: Andrew Cassidy
+Date: 9/17/2019
+Description: TESTS PROGRAM 1
+--------------------------------------------------------------------
+*/
+
+#include<iostream>
+
+using namespace std;
+
+// Function that takes set of inputs and runs program 1 len(input_list) times.
+// Will return pass / fail for each interation of the test. Will print a summary
+// of the testing.

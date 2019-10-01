5190fd391aac0a3725582284025ff39d9ab45df5
Andrew Cassidy <andrew.cassidy@uky.edu>
2019-09-30 22:07:59 -0400

    Update plab1test.cpp

--- plab1test.cpp
+++ plab1test.cpp
@@ -6,9 +6,38 @@
 */
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main() {
-    // Problem 1
+    // Problem 1 variables.
+    string stateInput;
+    // Problem 2 variables.
+    // Problem 3 variables.
+    // Problem 4 variables.
+
+    // Problem 1.
+    cout << "Question 1:" << endl;
+
+    // Get state name input.
+    cout << "Enter state abbreviation <KY, OH, IN, TN, WV>: ";
+    cin >> stateInput;
+
+    // stateInput input validation.
+    stateInput = toupper(stateInput);
+    if ((stateInput != "KY") & (stateInput != "OH") & (stateInput != "IN") & (stateInput != "TN") & (stateInput != "WV")) {
+        cout << "Invalid entry!" << endl;
+        cout << "Enter state abbreviation <KY, OH, IN, TN, WV>: ";
+        cin >> stateInput;
+        stateInput = toupper(stateInput);
+    }
+    else {
+        // Print out state associated with abbreviation.
+        if (stateInput == "KY") cout << "Kentucky" << endl;
+        else if (stateInput == "OH") cout << "Ohio" << endl;
+        else if (stateInput == "IN") cout << "Indiana" << endl;
+        else if (stateInput == "TN") cout << "Tennessee" << endl;
+        else cout << "West Virginia" << endl;
+    }
 }
